e456c40493f0e48529ef9618176237a8d96d11e2
vickiwarou <victoria.wattel@gmail.com>
2023-11-22 15:50:40 +0100

    yo

--- projet.c
+++ projet.c
@@ -42,15 +42,15 @@ int main(void){
 
     cage test_1_cage = {100, 100, 100, 50, 20};
     sheep test_1_sheep = {50, 50, 1, 1};
-
     //int dist_sheep_exit_X = abs(test_1_sheep.posX - test_1_cage.exitX);
     //int dist_sheep_exit_Y = abs(test_1_sheep.posY - test_1_cage.exitY);
 
     //double avancement_X->test_1_sheep.posX;
     //double avancement_Y = test_1_sheep.posY;
-    while((test_1_sheep.posX != test_1_cage.exitX)&&(test_1_sheep.posX != test_1_cage.exitY)){
+    while((test_1_sheep.posX <= test_1_cage.exitX)){
+        printf("yo");
         pathFinding(&test_1_sheep, test_1_cage);
-        printf("(%f : %f)\n", test_1_sheep.posX, test_1_sheep.posX);
+        printf("(%f:%f)\n", test_1_sheep.posX, test_1_sheep.posX);
     }
 
     return 0;
