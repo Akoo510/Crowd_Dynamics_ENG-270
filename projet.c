cf4497cb56fdc9e36169ea4a211b51a7d9876520
Akoo510 <tomjeannin@hotmail.fr>
2023-11-22 16:40:27 +0100

    com

--- projet.c
+++ projet.c
@@ -33,15 +33,17 @@ typedef struct obstacleCarre {
 
 int pathFinding(sheep *individu, cage enclos){
     double dx = individu->posX - enclos.exitX;
-    double dy = abs(individu->posY - enclos.exitY);
+    double dy = individu->posY - enclos.exitY;
     double dist = sqrt(dx*dx + dy*dy);
     double formerDist = dist;
     dist -= individu->speed;
     double dx2 = dist*abs(dx)/formerDist;
-    double dy2 = dist*dy/formerDist;
+    double dy2 = dist*abs(dy)/formerDist;
     individu->posX = enclos.exitX - dx2;
-    if (dx >= 0) individu->posY = enclos.exitY - dy2;
-    else if (dx < 0) individu->posY = enclos.exitY + dy2;
+    if (dx >= 0) individu->posY = enclos.exitY + dx2;
+    else if (dx < 0) individu->posY = enclos.exitY - dx2;
+    if (dy >= 0) individu->posY = enclos.exitY + dy2;
+    else if (dy < 0) individu->posY = enclos.exitY - dy2;
     return 0;
 }
 
@@ -49,7 +51,7 @@ int pathFinding(sheep *individu, cage enclos){
 int main(void){
 
     cage test_1_cage = {100, 100, 100, 50, 20};
-    sheep test_1_sheep = {0, 100, 1, 3};
+    sheep test_1_sheep = {0, 0, 1, 3};
     while((test_1_sheep.posX <= test_1_cage.exitX)){
         pathFinding(&test_1_sheep, test_1_cage);
         printf("(%f:%f)\n", test_1_sheep.posX, test_1_sheep.posY);
