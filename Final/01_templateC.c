da844f727ff68e3c7fdb4ebd0458e0eca1695711
Akoo510 <tomjeannin@hotmail.fr>
2023-12-20 14:23:15 +0100

    Commented Final

--- Final/01_templateC.c
+++ Final/01_templateC.c
@@ -5,6 +5,9 @@
 #include <math.h>
 #include <stdbool.h>
 
+#define LEFT_TRESHOLD 0.2
+#define RIGHT_TRESHOLD 0.8
+
 // Definition of Structure
 struct Force
 {
@@ -47,76 +50,75 @@ void seedRandomGenerator(unsigned int seed)
     srand(seed);
 }
 
-// Calculates the force that pushes the sheep towards the exit
-
 /// @brief Calculate on which side of the room is the door exit
-/// @return 0 : Left, 1 : Down, 2 : Right, 3 : Up
+/// @return 0 : Left-side, 1 : Down-side, 2 : Right-side, 3 : Up-side
 int roomExitSide()
 {
-    if (room.exitRoom.start.x == 0 && room.exitRoom.end.x == 0) return 0;
-    if (room.exitRoom.start.y == room.sizeRoom.x && room.exitRoom.end.y == room.sizeRoom.x) return 1;
-    if (room.exitRoom.start.x == room.sizeRoom.x && room.exitRoom.end.x == room.sizeRoom.x) return 2;
-    if (room.exitRoom.start.y == 0 && room.exitRoom.end.y == 0) return 3;
+    if (room.exitRoom.start.x == 0 && room.exitRoom.end.x == 0)
+        return 0;
+    if (room.exitRoom.start.y == room.sizeRoom.x && room.exitRoom.end.y == room.sizeRoom.x)
+        return 1;
+    if (room.exitRoom.start.x == room.sizeRoom.x && room.exitRoom.end.x == room.sizeRoom.x)
+        return 2;
+    if (room.exitRoom.start.y == 0 && room.exitRoom.end.y == 0)
+        return 3;
 
     // If none of the above conditions are met, then it should be impossible to reach this point
     return -1;
 }
 
-// Calculates the force that pushes the sheep towards the exit
-
-/// @brief Calculates the force applied on the sheep so that a goes to the exit, and returns it normalized
+/// @brief Calculates the force applied on the sheep so that it goes to the exit, and returns it normalized
 /// @param sheep Struct in which there'is a given x, y, radius, struct Force component, inRoom (bollean)
 void calculateForceTowardsExit(struct Sheep *sheep)
 {
     int exitSide = roomExitSide();
     double dx = room.exitRoom.end.x - room.exitRoom.start.x;
     double dy = room.exitRoom.end.y - room.exitRoom.start.y;
-    /* double t = ((sheep->x - room.exitRoom.start.x) * dx + (sheep->y - room.exitRoom.start.y) * dy) / (dx * dx + dy * dy);
-    t = (t < 0) ? 0 : (t > 1) ? 1 : t; */
-    struct Point closestPoint = {room.exitRoom.start.x + dx/2.0, room.exitRoom.start.y + dy / 2.0};
+    struct Point closestPoint = {room.exitRoom.start.x + dx / 2.0, room.exitRoom.start.y + dy / 2.0};
 
-    // If the sheep coordinate are between the dooor, they go to a "very far" location
-    
+    // If the sheep coordinate are between the dooor, they go to a "very far" location (like this we can see them exit)
     switch (exitSide)
     {
     case 0:
-        if (sheep->y >= room.exitRoom.start.y && sheep->y <= room.exitRoom.end.y && sheep->x < (room.sizeRoom.x * 0.2))
+        if (sheep->y >= room.exitRoom.start.y && sheep->y <= room.exitRoom.end.y && sheep->x < (room.sizeRoom.x * LEFT_TRESHOLD))
         {
-            closestPoint.x = -2 * room.sizeRoom.x;
+            closestPoint.x = -9 * room.sizeRoom.x;
         }
         break;
     case 1:
-        if (sheep->x >= room.exitRoom.start.x && sheep->x <= room.exitRoom.end.x && sheep->y > room.sizeRoom.y * 0.8)
+        if (sheep->x >= room.exitRoom.start.x && sheep->x <= room.exitRoom.end.x && sheep->y > (room.sizeRoom.y * RIGHT_TRESHOLD))
         {
-            closestPoint.y = 3 * room.sizeRoom.y;
+            closestPoint.y = 10 * room.sizeRoom.y;
         }
         break;
     case 2:
-        if (sheep->y >= room.exitRoom.start.y && sheep->y <= room.exitRoom.end.y && sheep->x > (room.sizeRoom.x * 0.8))
+        if (sheep->y >= room.exitRoom.start.y && sheep->y <= room.exitRoom.end.y && sheep->x > (room.sizeRoom.x * RIGHT_TRESHOLD))
         {
-            closestPoint.x = 3 * room.sizeRoom.x;
+            closestPoint.x = 10 * room.sizeRoom.x;
         }
         break;
     case 3:
-        if (sheep->x >= room.exitRoom.start.x && sheep->x <= room.exitRoom.end.x && sheep->y < (room.sizeRoom.y * 0.2))
+        if (sheep->x >= room.exitRoom.start.x && sheep->x <= room.exitRoom.end.x && sheep->y < (room.sizeRoom.y * LEFT_TRESHOLD))
         {
-            closestPoint.y = -2 * room.sizeRoom.y;
+            closestPoint.y = -9 * room.sizeRoom.y;
         }
         break;
     default:
         break;
     }
-    
+
     dx = closestPoint.x - sheep->x;
     dy = closestPoint.y - sheep->y;
     double distance = sqrt(dx * dx + dy * dy);
 
-    // The force is proportional to the distance to the exit
+    // Normalized force vector to exit
     sheep->force.fx = dx / distance;
     sheep->force.fy = dy / distance;
 }
 
-// Calculates the force that pushes the sheep away from the other sheep
+/// @brief Calculates the force that pushes the sheep away from the other sheep
+/// @param sheep1 A single element of the sheep struct
+/// @param sheep2 Another struct element to compare to
 void calculateForceBetweenSheep(struct Sheep *sheep1, struct Sheep *sheep2)
 {
     double dx = sheep2->x - sheep1->x;
@@ -147,12 +149,9 @@ void calculateForceBetweenSheep(struct Sheep *sheep1, struct Sheep *sheep2)
     }
 }
 
-void calculateForceWall()
-{
-
-}
-
-// Updates the position of a single sheep based on its velocity and acceleration
+/// @brief Updates the position of all the sheep (takes into account the sheep around and the coordinate of the exit)
+/// @param sheepArray the matrix of sheeps position and other parameter of the struct
+/// @param num_sheep the number of sheep in the room initially
 void updateForces(struct Sheep *sheepArray, int num_sheep)
 {
     for (int i = 0; i < num_sheep; ++i)
@@ -171,76 +170,93 @@ void updateForces(struct Sheep *sheepArray, int num_sheep)
     }
 }
 
-// Updates the positions of all sheeps and checks for collisions with walls or other sheeps
+/// @brief Checks if the sheep is still inside the room, and if not, updates the force so that it does not collide with the wall
+void handleExitSide0(struct Sheep *sheep, double newX)
+{
+    if (!(sheep->y > room.exitRoom.start.y && sheep->y < room.exitRoom.end.y) && (newX - sheep->r <= 0))
+        if (newX - sheep->r == 0)
+            sheep->force.fx = 0;
+        else
+            sheep->force.fx = sheep->x + sheep->r;
+    if ((sheep->x + sheep->r) < 0)
+        sheep->inRoom = false;
+}
+
+/// @brief Checks if the sheep is still inside the room, and if not, updates the force so that it does not collide with the wall
+void handleExitSide1(struct Sheep *sheep, double newY)
+{
+    if (!(sheep->x > room.exitRoom.start.x && sheep->x < room.exitRoom.end.x) && (newY + sheep->r >= room.sizeRoom.y))
+        if (newY + sheep->r == room.sizeRoom.y)
+            sheep->force.fy = 0;
+        else
+            sheep->force.fy = room.sizeRoom.y - (sheep->y + sheep->r);
+    if ((sheep->y - sheep->r) > room.sizeRoom.y)
+        sheep->inRoom = false;
+}
+
+/// @brief Checks if the sheep is still inside the room, and if not, updates the force so that it does not collide with the wall
+void handleExitSide2(struct Sheep *sheep, double newX)
+{
+    if (!(sheep->y > room.exitRoom.start.y && sheep->y < room.exitRoom.end.y) && (newX + sheep->r >= room.sizeRoom.x))
+        if (newX + sheep->r == room.sizeRoom.x)
+            sheep->force.fx = 0;
+        else
+            sheep->force.fx = room.sizeRoom.x - (sheep->x + sheep->r);
+    if ((sheep->x - sheep->r) < room.sizeRoom.x)
+        sheep->inRoom = false;
+}
+
+/// @brief Checks if the sheep is still inside the room, and if not, updates the force so that it does not collide with the wall
+void handleExitSide3(struct Sheep *sheep, double newY)
+{
+    if (!(sheep->x > room.exitRoom.start.x && sheep->x < room.exitRoom.end.x) && (newY - sheep->r <= 0))
+        if (newY - sheep->r == 0)
+            sheep->force.fy = 0;
+        else
+            sheep->force.fy = sheep->y + sheep->r;
+    if ((sheep->y + sheep->r) < 0)
+        sheep->inRoom = false;
+}
 
 /// @brief Updates the positions of all sheeps and checks for collisions with walls or other sheeps
-/// @param nbSheep The number of person
+/// @param nbSheep The number of Sheep
 /// @return An array of coordinate X and Y (in the struct format : Point)
-struct Point *moveSheep(int nbSheep) // POUR APRüS VISU : struct Point *fonction lololililolililol
+struct Point *moveSheep(int nbSheep)
 {
-    int exitSide = roomExitSide();
     struct Point *coordinates = malloc(nbSheep * sizeof(struct Point));
     updateForces(globalStructeArray, nbSheep);
-    
+
     for (int i = 0; i < nbSheep; ++i)
     {
         struct Sheep *sheep = &globalStructeArray[i];
-        if (!sheep->inRoom)
-            /* coordinates[i].x = 10.0;
-            coordinates[i].y = 10.0; */
-            continue;
         double newX = sheep->x + sheep->force.fx;
         double newY = sheep->y + sheep->force.fy;
-        
-        //Is the Sheep inside the room ?
-        switch (exitSide)
-        {
-        case 0:
-            if (!(sheep->y > room.exitRoom.start.y &&  sheep->y < room.exitRoom.end.y) && (newX - sheep->r <= 0))
-                if (newX - sheep->r == 0) 
-                    sheep->force.fx = 0;
-                else
-                    sheep->force.fx = sheep->x + sheep->r;
-            if ((sheep->x + sheep->r) < 0)
-                sheep->inRoom = false;
-            break;
-        case 1:
-            if (!(sheep->x > room.exitRoom.start.x &&  sheep->x < room.exitRoom.end.x) && (newY + sheep->r >= room.sizeRoom.y))
-                if (newY + sheep->r == room.sizeRoom.y)
-                    sheep->force.fy = 0;
-                else
-                    sheep->force.fy = room.sizeRoom.y - (sheep->y + sheep->r);
-            if ((sheep->y - sheep->r) > room.sizeRoom.y)
-                sheep->inRoom = false;
-            break;
-        case 2:
-            if (!(sheep->y > room.exitRoom.start.y &&  sheep->y < room.exitRoom.end.y) && (newX + sheep->r >= room.sizeRoom.x))
-            if  (newX + sheep->r == room.sizeRoom.x)
-                sheep->force.fx = 0;
-            else 
-                sheep->force.fx = room.sizeRoom.x - (sheep->x + sheep->r);
-            if ((sheep->x - sheep->r) < room.sizeRoom.x)   
-                sheep->inRoom = false;
-            break;
-        case 3:
-            if (!(sheep->x > room.exitRoom.start.x &&  sheep->x < room.exitRoom.end.x) && (newY - sheep->r <= 0))
-                if (newY - sheep->r == 0)
-                    sheep->force.fy = 0;
-                else
-                    sheep->force.fy = sheep->y + sheep->r;
-            if ((sheep->y + sheep->r) < 0)
-                sheep->inRoom = false;
-            break;
-        default:
-            break;
-        }
-        sheep->x += sheep->force.fx;
-        sheep->y += sheep->force.fy;
+
+        // If the sheep is inside the room, we update so that it does not collide with the wall
         if (sheep->inRoom)
         {
-            coordinates[i].x = globalStructeArray[i].x;
-            coordinates[i].y = globalStructeArray[i].y;
+            switch (roomExitSide())
+            {
+            case 0:
+                handleExitSide0(sheep, newX);
+                break;
+            case 1:
+                handleExitSide1(sheep, newY);
+                break;
+            case 2:
+                handleExitSide2(sheep, newX);
+                break;
+            case 3:
+                handleExitSide3(sheep, newY);
+                break;
+            default:
+                break;
+            }
         }
+
+        // We update the position in the coordinate matrix which will be send to the python code
+        coordinates[i].x = sheep->x += sheep->force.fx;
+        coordinates[i].y = sheep->y += sheep->force.fy;
     }
     return coordinates;
 }
@@ -276,7 +292,7 @@ void generateRandomSheeps(int nbBots, double radius)
         globalStructeArray[i].force.fx = 0;
         globalStructeArray[i].force.fy = 0;
         globalStructeArray[i].inRoom = true;
-        //printf("x1: %f, y1: %f\n", globalStructeArray[i].x, globalStructeArray[i].y);
+        // printf("x1: %f, y1: %f\n", globalStructeArray[i].x, globalStructeArray[i].y);
     }
 }
 
@@ -311,9 +327,6 @@ int sheepStillInside(int nbSheep)
     return 0;
 }
 
-// DO NOT FORGET THISSSS
-// pleasse, very important otherwise explosion
-
 /// @brief Allows to free a Matrix
 /// @param sheep Struct Sheep matrix to be freed
 void freeMatrix(struct Sheep sheep[])
@@ -321,29 +334,7 @@ void freeMatrix(struct Sheep sheep[])
     free(sheep);
 }
 
-int main()
-{
-    int nbSheep = 50;
-    seedRandomGenerator((unsigned int)time(NULL));
-    generateRoom(400.0, 400.0, 400.0, 190.0, 400.0, 210.0);
-    generateRandomSheeps(nbSheep, 1.0);
-    while (sheepStillInside(nbSheep))
-    {
-        struct Point *result = moveSheep(nbSheep);
-        
-        //Renvoie toute les coordonnées des points
-        /* for (int i = 0; i < nbSheep; i++)
-        {
-            printf("x %f, y %f\n", result[i].x, result[i].y);
-        }
-        free(result); */
-    }
-    freeMatrix(globalStructeArray);
-    return 0;
-}
-
 /*
 Compile with :
 gcc -shared -o 01_templateC.so 01_templateC.c
 */
-
